587d896d62925276124ff020d068ce5975f43d08
shadi <shadialaghbari@gmail.com>
2026-03-12 06:32:19 +0100

    Update welcome page content and formatting

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,9 @@
 @blockpart('header')
   <h1>Welcome</h1>
  
-  
+  <p>You do not need this at all if you were planing to make only Wordpress blocks.
+    Just go to Editor and add your blocks there. This is just an example of how you can use Acorn to make a custom page template with Livewire components.
+  </p>
   @livewire('hero')
-   @livewire('post-list')
+  @livewire('post-list')
 @endsection
