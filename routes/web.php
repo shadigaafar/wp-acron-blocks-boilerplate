a66a4d11d190530455ec2ce509f00c3eb246ed2f
shadi <shadialaghbari@gmail.com>
2026-03-12 06:32:30 +0100

    Remove unused routes from web.php

--- routes/web.php
+++ routes/web.php
@@ -7,10 +7,3 @@ Route::get('/welcome', function () {
     return view('welcome'); // resources/views/blocks/hero.blade.php
 });
 
-Route::get('/hi', function () {
-    return 'Hello from Acorn!';
-});
-
-Route::get('/test-hero', function () {
-    return \Livewire\Livewire::mount('hero')->html();
-});
